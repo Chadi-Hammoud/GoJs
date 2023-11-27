6ed1c493d6c5ae0928e4ce25a0a0007ca845657c
ibrahimdib <ibrahimdibpg19@gmail.com>
2023-11-27 14:47:07 +0200

    11/27/2023-2:46

--- BoardDesigner001/Js/index.mjs
+++ BoardDesigner001/Js/index.mjs
@@ -5,7 +5,8 @@ let myDiagram;
 
 function init() {
 
-  let b = parseInt(prompt("boards count"));
+  let borderCount = parseInt(prompt("boards count"));
+  let startIndex = parseInt(prompt("start Index, 0 or 1"));
 
   let $ = go.GraphObject.make;
 
@@ -17,11 +18,12 @@ function init() {
         new go.GridLayout(
           { wrappingWidth: Infinity, alignment: go.GridLayout.Position, cellSize: new go.Size(1, 1) }),
       "commandHandler.archetypeGroupData": { isGroup: true, text: "Group", horiz: false },
-      "undoManager.isEnabled": true
+      "undoManager.isEnabled": true,
+      //"fixedBounds": new go.Rect(0, 0, 800, 400), // Set fixedBounds to a specific rectangular area
     });
   // myDiagram.toolManager.resizingTool.computeReshape = function () { return false; }
 
-  function makeLayout(horiz) { // a Binding conversion function
+  function makeLayout(horiz) {
     if (horiz) {
       return new go.GridLayout(
         {
@@ -37,6 +39,7 @@ function init() {
     }
   }
 
+
   function defaultColor(horiz) { // a Binding conversion function
     return horiz ? "lightgray" : "lightgray";
   }
@@ -45,15 +48,7 @@ function init() {
     return horiz ? "bold 20px sans-serif" : "bold 16px sans-serif";
   }
 
-  // myDiagram.addDiagramListener("Modified", function(e) {
-  //   let board = e.subject.part;
-  //   let shelf = myDiagram.findNodeForKey("shelf");
-  //   if (board.actualBounds.intersects(shelf.actualBounds)) {
-  //     board.visible = false;
-  //   } else {
-  //     board.visible = true;
-  //   }
-  // });
+
 
   // myDiagram.groupTemplateMap.add("shelf",
   //   new go.Group("Auto",
@@ -62,8 +57,8 @@ function init() {
   //       background: "gray",
   //       //ungroupable: true,
   //       resizable: false,
-       
-        
+
+
   //     })
   //     //.bind("layout", "horiz", makeLayout)
   //    // .bind(new go.Binding("background", "isHighlighted", h => h ? "rgba(255,0,0,0.2)" : "transparent").ofObject())
@@ -94,87 +89,7 @@ function init() {
 
 
 
-  //   myDiagram.nodeTemplateMap.add("board",
-  //   $(go.Node, "Spot",
-  //     {
-  //       resizable: true,
-  //       resizeObjectName: "SHAPE",
-  //       //desiredSize: new go.Size(400, 130),
-  //     },
-  //     $(go.Panel, "Vertical",
 
-  //       $(go.Panel, "Horizontal",
-  //         {
-  //           click: (e, obj) => {
-  //             console.log("Port Clicked :", obj);
-  //           },
-  //           columnSizing: go.RowColumnDefinition.None,
-  //           height: 20,
-
-
-  //         },
-  //         $(go.Shape, "rectangle",
-  //           {
-  //             fill: "black",
-  //             alignment: go.Spot.Right,
-  //             stretch: go.GraphObject.None, // Don't stretch the white section
-  //             desiredSize: new go.Size(20, 20), // Set the desired size
-
-  //             margin: 0 // Set the padding to 0
-
-  //           },
-  //         ),
-  //         new go.Binding("width", "width", v => v / 4, null),
-
-  //         $(go.Shape, "rectangle",
-  //           {
-  //             fill: "white",
-  //             alignment: go.Spot.Center,
-  //             stretch: go.GraphObject.None, // Don't stretch the white section
-
-  //             maxSize: new go.Size(200, 20), // Set the max size
-  //             minSize: new go.Size(200, 20), // Set the min size
-  //             margin: 0 // Set the padding to 0
-  //           },
-  //           new go.Binding("width", "width", v => v / 2, null),
-  //         ),
-
-  //         $(go.Shape, "rectangle",
-  //           {
-  //             fill: "black",
-  //             alignment: go.Spot.Left,
-  //             stretch: go.GraphObject.None, // Don't stretch the white section
-
-  //             desiredSize: new go.Size(20, 20), 
-  //             margin: 0 // Set the padding to 0
-
-  //           },
-  //         ),
-  //         new go.Binding("width", "width", v => v / 4, null),
-  //       ),
-
-  //       $(go.Panel, {},
-  //         $(go.Shape, "rectangle",
-  //           {
-  //             //  name  :"SHAPE",
-  //             fill: "gray",
-  //             alignment: go.Spot.Left,
-  //             stretch: go.GraphObject.None, // Don't stretch the white section
-  //             desiredSize: new go.Size(242, 100), // Set the desired size
-
-  //             margin: 0, // Set the padding to 0
-
-  //           }
-  //         ),
-  //         new go.Binding("desiredSize", "size", null, new go.Size(200, 100)),
-  //         new go.Binding("width", "width", v => v / 4, null),
-  //       ),
-  //       new go.Binding("width", "width", null, null),
-
-  //     ),
-
-  //   )
-  // );
 
   //   go.Shape.defineFigureGenerator("headSection", function(shape, w, h) {
   //     var geo = new go.Geometry();
@@ -227,16 +142,30 @@ function init() {
             }
           ),
 
+          $(go.Panel, "Auto",
 
-          $(go.Shape, "Rectangle",
-            new go.Binding("width", "width", v => v * 0.83, null),
+            $(go.Shape, "Rectangle",
+              new go.Binding("width", "width", v => v * 0.83, null),
 
-            {
-              fill: "white",
-              stretch: go.GraphObject.Fill, // Make the shape resizable
-             
-            }
+              {
+                fill: "white",
+                stretch: go.GraphObject.Fill, // Make the shape resizable
+                
+              }
+            ),
+
+            $(go.TextBlock, "",
+              {
+                //text: "0:0",
+                margin: 3, // Add some margin to position the text inside the rectangle
+                //editable: true, // Make the text editable
+               
+                alignment: go.Spot.Left, // Center the text within the shape
+              },
+              new go.Binding("text", "text").makeTwoWay() // Bind the text to the 'text' property of the node data
+            )
           ),
+
           $(go.Shape, "Rectangle",
             new go.Binding("width", "width", v => v / 10, null),
             {
@@ -246,15 +175,7 @@ function init() {
             }
           ),
 
-          $(go.TextBlock,"",
-            {
-              margin: 5, // Add some margin to position the text inside the rectangle
-              editable: true, // Make the text editable
-              stroke: "black", // Set the text color to white
-              alignment: go.Spot.Center, // Center the text within the shape
-            },
-            new go.Binding("text", "text").makeTwoWay() // Bind the text to the 'text' property of the node data
-          )
+
         ),
         new go.Binding("width", "width").makeTwoWay(),
 
@@ -291,24 +212,21 @@ function init() {
 
   // Clear existing nodes
   myDiagram.model = new go.GraphLinksModel();
-  //myDiagram.model.addNodeData({ key: "boardGroup", isGroup: true, category: "shelf" }) ;
+  // myDiagram.model.addNodeData({ key: "boardGroup", isGroup: true, category: "shelf" }) ;
   // Add nodes dynamically based on the user input
-  let lay = prompt("is Vertical ?");
-  if(lay){
-    for (let i = 1; i <= b; i++) {
-      // myDiagram.model.addNodeData({ key: `port${i}`, group: "boardGroup", category: "board", width: 120, height: 120  });
-       myDiagram.model.addNodeData({ key: `port${i}`, category: "board", width: 120, height: 120  });
-   
-     }
-  }else{
+let k = 0;
 
-    for (let i = 1; i <= b; i++) {
-      // myDiagram.model.addNodeData({ key: `port${i}`, group: "boardGroup", category: "board", width: 120, height: 120  });
-       myDiagram.model.addNodeData({ key: `port${i}`, category: "board", width: 120, height: 120  });
-   
-     }
+
+  for (let i = 1; i <= borderCount; i++) {
+    
+    // myDiagram.model.addNodeData({ key: `port${i}`, group: "boardGroup", category: "board", width: 120, height: 120  });
+    myDiagram.model.addNodeData({ key: `port${i}`, category: "board", width: 120, height: 120, text: `${startIndex}:${k}` });
+    startIndex++;
   }
- 
+
+
+
+
 
 }
 
